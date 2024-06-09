27ab865f16520f39364069ef193c2b19b4ea3566
rzlm <razan4424@gmail.com>
2024-06-09 10:05:30 -0400

    education

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -12,9 +12,11 @@ const Education = () => {
 </h1> 
     </div>
     <div className='font-figtree text-txt-light text-center padding'>
-        <h1 className='text-2xl font-semibold'>I am in my 3rd year, working towards a BSc in Computer Science at Wilfrid Laurier University. </h1>
+        <h1 className='text-2xl font-semibold'>I am in my 3rd year, working towards a BSc in Computer Science at Wilfrid Laurier University (2022-2026)</h1>
         <p className='text-2xl font-semibold'>My concentration is in Big Data Systems.</p>
+        <p className='text-2xl font-semibold'>Courses in Data visualization, Software engineering, Operating Systems, Algorithm Design and Analysis, Data Structures</p>
         <p className='text-2xl font-semibold'>I am on the Dean's Honour Roll for the academic year of 2022, and 2023 (expected). GPA is 11.1/12.</p>
+        
     </div>
 
     </div>
